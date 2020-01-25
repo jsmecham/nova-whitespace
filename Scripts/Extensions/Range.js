03d961527ecd4f16396f8d3df22c0796615b894a
Justin Mecham <justin@guideline.com>
2020-01-24 20:26:03 -0500

    Added link to feature request for Range.prototype.length.

--- Scripts/Extensions/Range.js
+++ Scripts/Extensions/Range.js
@@ -5,6 +5,7 @@
 // Copyright © 2020 Justin Mecham. All rights reserved.
 //
 
+// See https://dev.panic.com/panic/nova-issues/issues/312
 if (typeof Range.prototype.length === "undefined") {
     Object.defineProperty(Range.prototype, "length", {
         get() {
