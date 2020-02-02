010b12d0060d547a147959bc1eb59984c89bbc22
Justin Mecham <justin@mecham.me>
2020-02-02 16:55:18 -0500

    Added EditorConfig as a dependency.

--- Source/Scripts/main.js
+++ Source/Scripts/main.js
@@ -5,13 +5,23 @@
 // Copyright © 2020 Justin Mecham. All rights reserved.
 //
 
+const { NPMExecutable } = require("nova-npm-executable");
 const { maybeTrimTrailingWhitespace, trimTrailingWhitespace } = require("./trimTrailingWhitespace");
 
 exports.activate = function() {
 
+    const editorConfig = new NPMExecutable("editorconfig");
+    if (!editorConfig.isInstalled) {
+		editorConfig.install().catch(error => {
+			console.error(error);
+		});
+	}
+
     // "Trim on Save"
     nova.subscriptions.add(
-        nova.workspace.onDidAddTextEditor(editor => editor.onWillSave(maybeTrimTrailingWhitespace))
+        nova.workspace.onDidAddTextEditor((editor) => {
+            return editor.onWillSave(maybeTrimTrailingWhitespace);
+        })
     );
 
 };
